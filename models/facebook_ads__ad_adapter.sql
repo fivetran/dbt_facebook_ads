d98c59f455c02b83efbf9e742fdf55554e914e0b
Dylan Baker <dylan@dbanalytics.co>
2020-12-17 11:43:44 -0500

    reporting models

---
+++ models/facebook_ads__ad_adapter.sql
@@ -0,0 +1,73 @@
+with report as (
+
+    select *
+    from {{ var('basic_ad') }}
+
+), creatives as (
+
+    select *
+    from {{ var('creative_history') }}
+
+), accounts as (
+
+    select *
+    from {{ var('account_history') }}
+
+), ads as (
+
+    select *
+    from {{ var('ad_history') }}
+
+), ad_sets as (
+
+    select *
+    from {{ var('ad_set_history') }}
+
+), campaigns as (
+
+    select *
+    from {{ var('campaign_history') }}
+
+), joined as (
+
+    select
+        report.date_day,
+        accounts.account_id,
+        accounts.account_name,
+        campaigns.campaign_id,
+        campaigns.campaign_name,
+        ad_sets.ad_set_id,
+        ad_sets.ad_set_name,
+        ads.ad_id,
+        ads.ad_name,
+        creatives.creative_id,
+        creatives.creative_name,
+        creatives.base_url,
+        creatives.url_host,
+        creatives.url_path,
+        creatives.utm_source,
+        creatives.utm_medium,
+        creatives.utm_campaign,
+        creatives.utm_content,
+        creatives.utm_term,
+        sum(report.clicks) as clicks,
+        sum(report.impressions) as impressions,
+        sum(report.spend) as spend
+    from report
+    left join ads 
+        on cast(report.ad_id as {{ dbt_utils.type_bigint() }}) = cast(ads.ad_id as {{ dbt_utils.type_bigint() }})
+    left join creatives
+        on cast(ads.creative_id as {{ dbt_utils.type_bigint() }}) = cast(creatives.creative_id as {{ dbt_utils.type_bigint() }})
+    left join ad_sets
+        on cast(ads.ad_set_id as {{ dbt_utils.type_bigint() }}) = cast(ad_sets.ad_set_id as {{ dbt_utils.type_bigint() }})
+    left join campaigns
+        on cast(ads.campaign_id as {{ dbt_utils.type_bigint() }}) = cast(campaigns.campaign_id as {{ dbt_utils.type_bigint() }})
+    left join accounts
+        on cast(report.account_id as {{ dbt_utils.type_bigint() }}) = cast(accounts.account_id as {{ dbt_utils.type_bigint() }})
+    {{ dbt_utils.group_by(19) }}
+
+
+)
+
+select *
+from joined
--- models/facebook_ads__ads.sql
+++
@@ -1,2 +0,0 @@
-select *
-from {{ ref('stg_facebook_ads__ad_history') }}
