cfe19cffcf846566ac7b8799fa858400f5d82aa0
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2022-12-08 11:05:29 -0600

    feature/enable-null-urls

--- models/facebook_ads__url_report.sql
+++ models/facebook_ads__url_report.sql
@@ -83,8 +83,15 @@ joined as (
     left join campaigns
         on ads.campaign_id = campaigns.campaign_id
     left join accounts
-        on report.account_id = accounts.account_id    
+        on report.account_id = accounts.account_id  
+
+    {% if   (var('include_facebook_ads_null_urls', False)) or
+            (var('include_ad_reporting_null_urls', False))  %}
+        -- In this case, skip where clause to include all rows whether or not the url field is populated.
+    {% else %}  
     where creatives.url is not null
+    {% endif %}
+    
     {{ dbt_utils.group_by(19) }}
 )
 
