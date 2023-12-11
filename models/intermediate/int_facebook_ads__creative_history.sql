d576be922cdd43730a6884a8885a5f4b8fca5eb0
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2023-12-11 10:58:09 -0600

    Update int_facebook_ads__creative_history.sql

--- models/intermediate/int_facebook_ads__creative_history.sql
+++ models/intermediate/int_facebook_ads__creative_history.sql
@@ -44,11 +44,11 @@ fields as (
         {{ dbt.split_part(url_field, "'?'", 1) }} as base_url,
         {{ dbt_utils.get_url_host(url_field) }} as url_host,
         '/' || {{ dbt_utils.get_url_path(url_field) }} as url_path,
-        coalesce(url_tags_pivoted.utm_source, {{ dbt_utils.get_url_parameter(url_field, 'utm_source') }}) as utm_source,
-        coalesce(url_tags_pivoted.utm_medium, {{ dbt_utils.get_url_parameter(url_field, 'utm_medium') }}) as utm_medium,
-        coalesce(url_tags_pivoted.utm_campaign, {{ dbt_utils.get_url_parameter(url_field, 'utm_campaign') }}) as utm_campaign,
-        coalesce(url_tags_pivoted.utm_content, {{ dbt_utils.get_url_parameter(url_field, 'utm_content') }}) as utm_content,
-        coalesce(url_tags_pivoted.utm_term, {{ dbt_utils.get_url_parameter(url_field, 'utm_term') }}) as utm_term
+        coalesce(url_tags_pivoted.utm_source, {{ facebook_ads.facebook_ads_extract_url_parameter(url_field, 'utm_source') }}) as utm_source,
+        coalesce(url_tags_pivoted.utm_medium, {{ facebook_ads.facebook_ads_extract_url_parameter(url_field, 'utm_medium') }}) as utm_medium,
+        coalesce(url_tags_pivoted.utm_campaign, {{ facebook_ads.facebook_ads_extract_url_parameter(url_field, 'utm_campaign') }}) as utm_campaign,
+        coalesce(url_tags_pivoted.utm_content, {{ facebook_ads.facebook_ads_extract_url_parameter(url_field, 'utm_content') }}) as utm_content,
+        coalesce(url_tags_pivoted.utm_term, {{ facebook_ads.facebook_ads_extract_url_parameter(url_field, 'utm_term') }}) as utm_term
     from base
     left join url_tags_pivoted
         on base._fivetran_id = url_tags_pivoted._fivetran_id
