97717edf918cf203fae3383073381e063fb6c363
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2025-03-13 13:14:58 -0700

    Update transform package to incorporate `creative_history.url_tags` source package JSON fix (#49)
    * test on bk
    
    * pr polishes
    
    * update license
    
    * add license to changelog
    
    * regen docs due to source update
    
    * package ref

--- integration_tests/tests/consistency/consistency_ad_report.sql
+++ integration_tests/tests/consistency/consistency_ad_report.sql
@@ -8,9 +8,9 @@ with prod as (
         ad_id,
         sum(coalesce(clicks, 0)) as clicks, 
         sum(coalesce(impressions, 0)) as impressions,
-        sum(coalesce(spend, 0)) as spend
-        {# sum(coalesce(conversions_value, 0)) as conversions_value,
-        sum(coalesce(conversions, 0)) as conversions #}
+        sum(coalesce(spend, 0)) as spend,
+        sum(coalesce(conversions_value, 0)) as conversions_value,
+        sum(coalesce(conversions, 0)) as conversions
     from {{ target.schema }}_facebook_ads_prod.facebook_ads__ad_report
     group by 1
 ),
@@ -20,9 +20,9 @@ dev as (
         ad_id,
         sum(coalesce(clicks, 0)) as clicks, 
         sum(coalesce(impressions, 0)) as impressions,
-        sum(coalesce(spend, 0)) as spend
-        {# sum(coalesce(conversions_value, 0)) as conversions_value,
-        sum(coalesce(conversions, 0)) as conversions #}
+        sum(coalesce(spend, 0)) as spend,
+        sum(coalesce(conversions_value, 0)) as conversions_value,
+        sum(coalesce(conversions, 0)) as conversions
     from {{ target.schema }}_facebook_ads_dev.facebook_ads__ad_report
     group by 1
 ),
@@ -35,11 +35,11 @@ final as (
         prod.impressions as prod_impressions,
         dev.impressions as dev_impressions,
         prod.spend as prod_spend,
-        dev.spend as dev_spend
-        {# prod.conversions_value as prod_conversions_value,
+        dev.spend as dev_spend,
+        prod.conversions_value as prod_conversions_value,
         dev.conversions_value as dev_conversions_value,
         prod.conversions as prod_conversions,
-        dev.conversions as dev_conversions, #}
+        dev.conversions as dev_conversions
     from prod
     full outer join dev 
         on dev.ad_id = prod.ad_id
@@ -51,5 +51,5 @@ where
     abs(prod_clicks - dev_clicks) >= .01
     or abs(prod_impressions - dev_impressions) >= .01
     or abs(prod_spend - dev_spend) >= .01
-    {# or abs(prod_conversions_value - dev_conversions_value) >= .01
-    or abs(prod_conversions - dev_conversions) >= .01 #}
+    or abs(prod_conversions_value - dev_conversions_value) >= .01
+    or abs(prod_conversions - dev_conversions) >= .01
