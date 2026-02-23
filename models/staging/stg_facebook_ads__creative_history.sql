cf910acd0bb97601be42927b7c109c1f78711f51
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-02-23 15:57:31 -0600

    fix get cols

--- models/staging/stg_facebook_ads__creative_history.sql
+++ models/staging/stg_facebook_ads__creative_history.sql
@@ -11,7 +11,7 @@ fields as (
     select
         {{
             fivetran_utils.fill_staging_columns(
-                source_columns=columns,
+                source_columns=adapter.get_columns_in_relation(ref('stg_facebook_ads__creative_history_tmp')),
                 staging_columns=get_creative_history_columns()
             )
         }}
