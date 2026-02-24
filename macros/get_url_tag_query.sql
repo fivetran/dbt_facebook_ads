ef2f274a33aa417ca59c5f7b9aaba1474154c856
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-02-24 10:48:37 -0600

    cleanup

--- macros/get_url_tag_query.sql
+++ macros/get_url_tag_query.sql
@@ -3,7 +3,6 @@
 {%- endmacro %}
 
 {%- macro default__get_url_tags_query() %}
-
     fields as (
         select
             source_relation,
@@ -14,245 +13,140 @@
             cast(null as {{ dbt.type_string() }}) as type
         from required_fields
     )
-
 {%- endmacro %}
 
 {%- macro bigquery__get_url_tags_query() %}
 
-    {%- set url_tags_datatype = get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') %}
-    {%- if url_tags_datatype == 'json' %}
+    {%- set is_native_json = get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') == 'json' %}
 
-        unnested as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                json_value
-            from required_fields,
-            unnest(json_extract_array(url_tags)) as json_value
-            where url_tags is not null
-        ),
+    unnested as (
+        select
+            source_relation,
+            _fivetran_id,
+            creative_id,
+            url_tag_element
+        from required_fields,
 
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                json_extract_scalar(json_value, '$.key') as key,
-                json_extract_scalar(json_value, '$.value') as value,
-                json_extract_scalar(json_value, '$.type') as type
-            from unnested
-        )
-
+    {%- if is_native_json %}
+        unnest(url_tags) as url_tag_element
     {%- else %}
-
-        cleaned_json as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                json_extract_array(replace(trim(url_tags, '"'),'\\','')) as cleaned_url_tags
-            from required_fields
-        ), 
-
-        unnested as (
-            select 
-                source_relation,
-                _fivetran_id, 
-                creative_id, 
-                url_tag_element
-            from cleaned_json
-            left join unnest(cleaned_url_tags) as url_tag_element
-            where cleaned_url_tags is not null
-        ), 
-
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                json_extract_scalar(url_tag_element, '$.key') as key,
-                json_extract_scalar(url_tag_element, '$.value') as value,
-                json_extract_scalar(url_tag_element, '$.type') as type
-            from unnested
-        )
-    {%- endif%}
-    {%- endmacro %}
-
-
-    {%- macro postgres__get_url_tags_query() %}
-
-    {%- set url_tags_datatype = get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') %}
-    {%- if url_tags_datatype in ('json', 'jsonb') %}
-
-        unnested as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                url_tag_element
-            from required_fields
-            left join lateral json_array_elements(url_tags) as url_tag_element on True
-            where url_tags is not null
-        ),
-
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                url_tag_element->>'key' as key,
-                url_tag_element->>'value' as value,
-                url_tag_element->>'type' as type
-            from unnested
-        )
-
-    {%- else %}
-
-        cleaned_json as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                replace(trim(url_tags::text, '"'),'\\','')::json as cleaned_url_tags
-            from required_fields
-        ),
-
-        unnested as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                url_tag_element
-            from cleaned_json
-            left join lateral json_array_elements(cleaned_url_tags) as url_tag_element on True
-            where cleaned_url_tags is not null
-        ),
-
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                url_tag_element->>'key' as key,
-                url_tag_element->>'value' as value,
-                url_tag_element->>'type' as type
-            from unnested
-        )
+        unnest(json_extract_array(replace(trim(url_tags, '"'),'\\',''))) as url_tag_element
     {%- endif %}
-    {%- endmacro %}
 
+        where url_tags is not null
+    ),
 
-    {%- macro redshift__get_url_tags_query() %}
+    fields as (
+        select
+            source_relation,
+            _fivetran_id,
+            creative_id,
+            json_extract_scalar(url_tag_element, '$.key') as key,
+            json_extract_scalar(url_tag_element, '$.value') as value,
+            json_extract_scalar(url_tag_element, '$.type') as type
+        from unnested
+    )
+{%- endmacro %}
 
-    {%- set url_tags_datatype = get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') %}
-    {%- if url_tags_datatype in ('json', 'super') %}
+{%- macro postgres__get_url_tags_query() %}
 
-        numbers as (
-            {{ dbt_utils.generate_series(upper_bound=1000) }}
-        ),
+    {%- set is_native_json = get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') in ('json', 'jsonb') %}
 
-        flattened_url_tags as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                url_tags[numbers.generated_number::int - 1] as element
-            from required_fields
-            inner join numbers
-                on json_array_length(url_tags) >= numbers.generated_number
-        ),
+    unnested as (
+        select
+            source_relation,
+            _fivetran_id,
+            creative_id,
+            url_tag_element
+        from required_fields
 
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                element.key::varchar as key,
-                element.value::varchar as value,
-                element.type::varchar as type
-            from flattened_url_tags
-        )
-
+    {%- if is_native_json %}
+        left join lateral json_array_elements(url_tags) as url_tag_element on True
     {%- else %}
+        left join lateral json_array_elements(replace(trim(url_tags::text, '"'),'\\','')::json) as url_tag_element on True
+    {%- endif %}
 
-        numbers as (
-            {{ dbt_utils.generate_series(upper_bound=1000) }}
-        ),
+        where url_tags is not null
+    ),
 
-        flattened_url_tags as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
+    fields as (
+        select
+            source_relation,
+            _fivetran_id,
+            creative_id,
+            url_tag_element->>'key' as key,
+            url_tag_element->>'value' as value,
+            url_tag_element->>'type' as type
+        from unnested
+    )
+{%- endmacro %}
+
+{%- macro redshift__get_url_tags_query() %}
+
+    {%- set is_native_json = get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') == 'super' %}
+
+    numbers as (
+        {{ dbt_utils.generate_series(upper_bound=1000) }}
+    ),
+
+    flattened_url_tags as (
+        select
+            source_relation,
+            _fivetran_id,
+            creative_id,
+
+        {%- if is_native_json %}
+                required_fields.url_tags[numbers.generated_number::int - 1] as element
+        {%- else %}
                 json_extract_array_element_text(required_fields.url_tags, numbers.generated_number::int - 1, true) as element
+        {%- endif %}
             from required_fields
             inner join numbers
                 on json_array_length(required_fields.url_tags) >= numbers.generated_number
-        ),
+    ),
 
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                json_extract_path_text(element,'key') as key,
-                json_extract_path_text(element,'value') as value,
-                json_extract_path_text(element,'type') as type
-            from flattened_url_tags
-        )
-    {%- endif %}
-    {%- endmacro %}
+    fields as (
+        select
+            source_relation,
+            _fivetran_id,
+            creative_id,
 
+        {%- if is_native_json %}
+            element.key::varchar as key,
+            element.value::varchar as value,
+            element.type::varchar as type
+        {%- else %}
+            json_extract_path_text(element,'key') as key,
+            json_extract_path_text(element,'value') as value,
+            json_extract_path_text(element,'type') as type
+        {%- endif %}
 
-    {%- macro snowflake__get_url_tags_query() %}
+        from flattened_url_tags
+    )
+{%- endmacro %}
 
-    {%- set url_tags_datatype = get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') %}
-    {%- if url_tags_datatype in ('variant', 'object', 'array') %}
+{%- macro snowflake__get_url_tags_query() %}
 
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                url_tags.value:key::string as key,
-                url_tags.value:value::string as value,
-                url_tags.value:type::string as type
-            from required_fields,
-            lateral flatten( input => url_tags ) as url_tags
-            where url_tags is not null
-        )
+    {%- set is_native_json = 
+        get_column_datatype('stg_facebook_ads__creative_history', 'url_tags') in ('variant', 'object', 'array') %}
 
-    {%- else %}
+    fields as (
+        select
+            source_relation,
+            _fivetran_id,
+            creative_id,
+            url_tags.value:key::string as key,
+            url_tags.value:value::string as value,
+            url_tags.value:type::string as type
+        from required_fields,
+        lateral flatten(input => {{ 'url_tags' if is_native_json else 'parse_json(url_tags)' }}) as url_tags
+        where url_tags is not null
+    )
+{%- endmacro %}
 
-        cleaned_fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                parse_json(url_tags) as url_tags
-            from required_fields
-            where url_tags is not null
-        ),
-
-        fields as (
-            select
-                source_relation,
-                _fivetran_id,
-                creative_id,
-                url_tags.value:key::string as key,
-                url_tags.value:value::string as value,
-                url_tags.value:type::string as type
-            from cleaned_fields,
-            lateral flatten( input => url_tags ) as url_tags
-        )
-    {%- endif %}
-    {%- endmacro %}
-
-
-    {%- macro spark__get_url_tags_query() %}
+{%- macro spark__get_url_tags_query() %}
     {# JSON datatype not supported by Fivetran so no need to check datatype. #}
-    
+
     cleaned_fields as (
         select
             source_relation,
@@ -261,7 +155,7 @@
             explode(from_json(url_tags, 'array<struct<key:STRING, value:STRING, type:STRING>>')) as url_tags
         from required_fields
         where url_tags is not null
-    ), 
+    ),
 
     fields as (
         select
@@ -273,5 +167,4 @@
             url_tags.type as type
         from cleaned_fields
     )
-
 {%- endmacro %}
